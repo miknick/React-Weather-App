eb388aa54c4b9527f293bbf2280c0ac7f343650c
Yunus Akten <yunusakten2@gmail.com>
2020-08-16 20:21:20 +0300

    Updates

---
+++ precache-manifest.a2fbf193ca8c35d95015a90c82ba78b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3908f52f011e12da779de0e1097d31ca",
+    "url": "/React-Weather-App/index.html"
+  },
+  {
+    "revision": "6c871e64ec7e9df5dc76",
+    "url": "/React-Weather-App/static/css/main.320b9023.chunk.css"
+  },
+  {
+    "revision": "aadc7324442fb4cdcde7",
+    "url": "/React-Weather-App/static/js/2.b43871f4.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/React-Weather-App/static/js/2.b43871f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c871e64ec7e9df5dc76",
+    "url": "/React-Weather-App/static/js/main.66326fc4.chunk.js"
+  },
+  {
+    "revision": "687762adf352874aaef0",
+    "url": "/React-Weather-App/static/js/runtime-main.3bd7d47a.js"
+  },
+  {
+    "revision": "f8cb08cf968bc8343b6469d77ab4ff8d",
+    "url": "/React-Weather-App/static/media/elio-santos-B6hw9ooyldM-unsplash.f8cb08cf.jpg"
+  }
+]);
